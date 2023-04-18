9cb1000d8694b5afc6496aa356b7090802a0f51f
Bulela Gomoshe <bulelagomoshe1@gmail.com>
2023-04-18 09:23:02 +0200

    wtc if statements

---
+++ basics.py
@@ -0,0 +1,57 @@
+
+
+
+# my_number = 42
+
+# your_guess = 0
+
+# while (your_guess != my_number):
+
+#     your_input = input("Guess my number: ")
+#     your_guess = int(your_input)
+# if your_guess < my_number:
+#         print("guess higher!")
+# elif your_guess > my_number: 
+          
+#             print("guess lower!")
+# print("You got it!")
+
+
+
+# print("Starting...")
+# count = 0
+# while count < 17: # repeat 10 times
+#     print(count)
+#     count = count + 1
+# print("Done!")
+
+
+
+
+
+
+# def example_3():
+#     for count in range(0,10): # we start at 0 and end at 9 (not 10!)
+#         print(count)
+
+# example_3()
+
+# my_number = 42
+# while True: # The loop condition is always true
+#     your_input = input("Guess my number: ")
+#     your_guess = int(your_input)
+#     if your_guess == my_number:
+#         break # and we break out of the loop here
+#     if your_guess < my_number:
+#         print("guess higher!")
+#     else:
+#         print("guess lower!")
+# print("You got it!")
+
+
+
+range(5,15)
+
+# range(20)
+
+# range(0,101,10)? What happened?
